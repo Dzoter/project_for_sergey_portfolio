a2ef904371bb646fb81c02d586c4f07c030390cb
Dzoter <Andry94@list.ru>
2022-08-29 19:08:35 +0400

    Сделал шаблончики blade

---
+++ resources/views/mainContent/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.layout')
+@section('content')
+
+@yield('about', View::make('sections.about'))
+@yield('about')
+
+@yield('facts', View::make('sections.facts'))
+@yield('facts')
+
+@yield('skills', View::make('sections.skills'))
+@yield('skills')
+
+@yield('resume', View::make('sections.resume'))
+@yield('resume')
+
+@yield('portfolio', View::make('sections.portfolio'))
+@yield('portfolio')
+
+@yield('services', View::make('sections.services'))
+@yield('services')
+
+@yield('testimonials', View::make('sections.testimonials'))
+@yield('testimonials')
+
+@yield('contact', View::make('sections.contact'))
+@yield('contact')
+@endsection
